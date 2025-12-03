585557710e326fd3ce3d2e71a88e5e5a1669c1b0
wk-ahao <1260413908@qq.com>
2025-12-04 01:11:07 +0800

    fix white screen issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // <--- 加上这一行！意思是使用相对路径
   plugins: [react()],
-  base: '/my-expense-app/', // 这里要和你的仓库名一致
-})
+});
